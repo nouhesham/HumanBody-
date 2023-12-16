7ad4d6aa01071e3fdf6442e1e0a16fce25c34936
Nourhan hesham <nourhanhesham263@gmail.com>
2023-12-17 00:48:13 +0200

    adding animation,refractoring the code

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 "use strict";
-
-// var controller = new ScrollMagic.Controller();
-// var scene = new ScrollMagic.Scene({
-//   triggerElement: "#scrolled", // element to trigger the pin
-//   duration: 300, // duration of the pin (in px)
-// })
-//   .setPin("#scrolled") // element to pin
-//   .addTo(controller);
+AOS.init();
 
 const bodyColumnButtons = document.querySelectorAll(".bodyColumn__button");
-const Circle = document.querySelectorAll(".circle");
+const circles = document.querySelectorAll(".circle");
 
-console.log(bodyColumnButtons);
-console.log(Circle);
-
-const HandleEvent = (event) => {
+const handleEvent = (event) => {
   bodyColumnButtons.forEach((button) => {
     if (button !== event.target) {
       button.classList.remove("Red");
     }
-    Circle.forEach((circle) => {
+  });
+
+  circles.forEach((circle) => {
+    if (circle.id === event.target.dataset.id) {
+      circle.classList.add("SpottedCircle");
+    } else {
+      circle.classList.remove("SpottedCircle");
+    }
+  });
+
+  event.target.classList.add("Red");
+};
+
+const handleBodyEvent = (event) => {
+  circles.forEach((circle) => {
+    bodyColumnButtons.forEach((button) => {
+      if (event.target.id === button.dataset.id) {
+        event.target.classList.add("SpottedCircle");
+        button.classList.add("Red");
+      } else {
+        button.classList.remove("Red");
+      }
+
       if (circle !== event.target) {
         circle.classList.remove("SpottedCircle");
       }
-      if (circle.id === event.target.dataset.id) {
-        circle.classList.add("SpottedCircle");
-      }
     });
   });
-  event.target.classList.add("Red");
-};
-const HandleBodyEvent = (event) => {
-  Circle.forEach((circle) => {});
 };
 
-bodyColumnButtons.forEach((button) =>
-  button.addEventListener("click", HandleEvent)
-);
-Circle.forEach((circle) => {
-  circle.addEventListener("click", HandleBodyEvent);
+bodyColumnButtons.forEach((button) => {
+  button.addEventListener("click", handleEvent);
+});
+
+circles.forEach((circle) => {
+  circle.addEventListener("click", handleBodyEvent);
 });
